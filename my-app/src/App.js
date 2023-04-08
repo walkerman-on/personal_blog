a6bb9c56ff0385fcfe81b4d976a5a0cec1873eb2
Alexey Groshev <82907949+walkerman-on@users.noreply.github.com>
2023-04-08 10:32:49 +0300

    Start learning React

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Counter from "./components/Counter"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Counter/>
+      <Counter/>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App;
