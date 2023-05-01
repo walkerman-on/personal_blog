8f82b8fab49b3b63374cd69d3675503c08ecbe48
Alexey Groshev <82907949+walkerman-on@users.noreply.github.com>
2023-05-01 21:15:26 +0300

    Making PostForm component with callback

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,50 +1,27 @@
-import React, {useState, useRef} from "react"
+import React, {useState} from "react"
 import "./components/styles/App.css"
 import ListItem from "./components/ListItem";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm"
 
 function App() {
 
-  const [posts, setPost] = useState( [
+  const [posts, setPosts] = useState( [
     {id: 1, title: "Javascript 1", description: "Описание"},
     {id: 2, title: "Javascript 2", description: "Описание"},
     {id: 3, title: "Javascript 3", description: "Описание"},
   ])
 
-  const addNewPost = (event) => {
-    event.preventDefault()
-    const newPost = {
-      id: Date.now(),
-      title,
-      body
-    }
-    console.log(newPost)
-    setPost([...posts, newPost])
-    setTitle("")
-    setBody("")
+  //Функция обратного вызова в PostForm
+  const createForm = (newPost) => {
+    setPosts([...posts, newPost])
   }
-
-  const [title, setTitle] = useState("")
-  const [body, setBody] = useState("")
 
 
   return (
     <div className="app">
-      <form className="formMain">
-        <MyInput
-            value = {title}
-            onChange = {event => setTitle(event.target.value)}
-            type = "text" 
-            placeholder="Название поста"/>
-        <MyInput
-            value = {body}
-            onChange = {event => setBody(event.target.value)}
-            type = "text" 
-            placeholder="Описание поста"/>
-      
-        <MyButton onClick = {addNewPost}>Создать пост</MyButton>
-      </form>
+      <PostForm create = {createForm}/>
       <ListItem post = {posts} title = "Список постов 1"/>
     </div>
   )
